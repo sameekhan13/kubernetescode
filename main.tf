757b6ba901616da8cd6c6f97ace92de0b7689a93
sameekhan13 <44860480+sameekhan13@users.noreply.github.com>
2023-01-06 17:17:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-provider "aws" {
-    region = "eu-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
 
 resource "vpcname" "my_vpc" {
     cidr_block = "0.0.0.0/16"
+    region = "eu-west-2"
 }
